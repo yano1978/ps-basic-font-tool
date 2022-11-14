d2305a2af392fc829eae5411f3e9ac3621c19fa7
yano1978 <yano1978@gmail.com>
2022-11-14 10:14:09 +0000

    Base added for test building

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://yano1978.github.io/ps-basic-font-tool/',
+  // base: 'http://ernestoianuario.com/ps-basic/',
   plugins: [react()],
 });
