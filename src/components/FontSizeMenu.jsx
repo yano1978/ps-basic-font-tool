e4fd5cc1be1c0140622efed19a4ce831b4a72842
yano1978 <yano1978@gmail.com>
2022-12-07 14:55:23 +0000

    data structure for all fonts added + loop columns OK + first columns CSS styling WIP

--- src/components/FontSizeMenu.jsx
+++ src/components/FontSizeMenu.jsx
@@ -15,7 +15,6 @@ const DropdownMenu = ({ className = '', toggleWeight }) => {
       id: 1,
       fontTitle: '440, light',
       fontClass: '',
-      fontClicked: false,
     },
     {
       id: 2,
@@ -27,123 +26,332 @@ const DropdownMenu = ({ className = '', toggleWeight }) => {
       id: 3,
       fontTitle: '450, semi-light',
       fontClass: '',
-      fontClicked: false,
     },
     {
       id: 4,
       fontTitle: '454, semi-light italic',
       fontClass: '',
-      fontClicked: false,
     },
     {
       id: 5,
       fontTitle: '460, regular',
       fontClass: '',
-      fontClicked: false,
     },
     {
       id: 6,
       fontTitle: '464, regular italic',
       fontClass: '',
-      fontClicked: false,
     },
     {
       id: 7,
       fontTitle: '470, medium',
       fontClass: '',
-      fontClicked: false,
     },
     {
       id: 8,
       fontTitle: '474, medium italic',
       fontClass: '',
-      fontClicked: false,
     },
     {
       id: 9,
       fontTitle: '480, semi-bold',
       fontClass: '',
-      fontClicked: false,
     },
     {
       id: 10,
       fontTitle: '484, semi-bold italic',
       fontClass: '',
-      fontClicked: false,
     },
     {
       id: 11,
       fontTitle: '490, bold',
       fontClass: '',
-      fontClicked: false,
     },
     {
       id: 12,
       fontTitle: '494, bold italic',
       fontClass: '',
+    },
+  ]);
+  const [compactFonts] = useState([
+    {
+      id: 13,
+      fontTitle: '540, light',
+      fontClass: '',
+    },
+    {
+      id: 14,
+      fontTitle: '544, light italic',
+      fontClass: '',
       fontClicked: false,
+    },
+    {
+      id: 15,
+      fontTitle: '550, semi-light',
+      fontClass: '',
+    },
+    {
+      id: 16,
+      fontTitle: '554, semi-light italic',
+      fontClass: '',
+    },
+    {
+      id: 17,
+      fontTitle: '560, regular',
+      fontClass: '',
+    },
+    {
+      id: 18,
+      fontTitle: '564, regular italic',
+      fontClass: '',
+    },
+    {
+      id: 19,
+      fontTitle: '570, medium',
+      fontClass: '',
+    },
+    {
+      id: 20,
+      fontTitle: '574, medium italic',
+      fontClass: '',
+    },
+    {
+      id: 21,
+      fontTitle: '580, semi-bold',
+      fontClass: '',
+    },
+    {
+      id: 22,
+      fontTitle: '584, semi-bold italic',
+      fontClass: '',
+    },
+    {
+      id: 23,
+      fontTitle: '590, bold',
+      fontClass: '',
+    },
+    {
+      id: 24,
+      fontTitle: '594, bold italic',
+      fontClass: '',
     },
   ]);
   const [normalFonts] = useState([
     {
-      id: 13,
+      id: 25,
       fontTitle: '640, light',
       fontClass: 'PSBasic1200-640Light',
     },
     {
-      id: 14,
+      id: 26,
       fontTitle: '644, light italic',
       fontClass: 'PSBasic1200-644LightItalic',
     },
     {
-      id: 15,
+      id: 27,
       fontTitle: '650, semi-light',
       fontClass: 'PSBasic1200-650Semi-Light',
     },
     {
-      id: 16,
+      id: 28,
       fontTitle: '654, semi-light italic',
       fontClass: 'PSBasic1200-654Semi-LightItalic',
     },
     {
-      id: 17,
+      id: 29,
       fontTitle: '660, regular',
       fontClass: 'PSBasic1200-660Regular',
     },
     {
-      id: 18,
+      id: 30,
       fontTitle: '664, regular italic',
       fontClass: 'PSBasic1200-664RegularItalic',
     },
     {
-      id: 19,
+      id: 31,
       fontTitle: '670, medium',
       fontClass: 'PSBasic1200-670Medium',
     },
     {
-      id: 20,
+      id: 32,
       fontTitle: '674, medium italic',
       fontClass: 'PSBasic1200-674MediumItalic',
     },
     {
-      id: 21,
+      id: 33,
       fontTitle: '680, semi-bold',
       fontClass: 'PSBasic1200-680Semi-Bold',
     },
     {
-      id: 22,
+      id: 34,
       fontTitle: '684, semi-bold italic',
       fontClass: 'PSBasic1200-684Semi-BoldItalic',
     },
     {
-      id: 23,
+      id: 35,
       fontTitle: '690, bold',
       fontClass: 'PSBasic1200-690Bold',
     },
     {
-      id: 24,
+      id: 36,
       fontTitle: '694, bold italic',
       fontClass: 'PSBasic1200-694BoldItalic',
+    },
+  ]);
+  const [extendedFonts] = useState([
+    {
+      id: 37,
+      fontTitle: '740, light',
+      fontClass: 'PSBasic1200-740Light',
+    },
+    {
+      id: 38,
+      fontTitle: '744, light italic',
+      fontClass: 'PSBasic1200-744LightItalic',
+    },
+    {
+      id: 39,
+      fontTitle: '750, semi-light',
+      fontClass: 'PSBasic1200-750Semi-Light',
+    },
+    {
+      id: 40,
+      fontTitle: '754, semi-light italic',
+      fontClass: 'PSBasic1200-754Semi-LightItalic',
+    },
+    {
+      id: 41,
+      fontTitle: '760, regular',
+      fontClass: 'PSBasic1200-760Regular',
+    },
+    {
+      id: 42,
+      fontTitle: '764, regular italic',
+      fontClass: 'PSBasic1200-764RegularItalic',
+    },
+    {
+      id: 43,
+      fontTitle: '770, medium',
+      fontClass: 'PSBasic1200-770Medium',
+    },
+    {
+      id: 44,
+      fontTitle: '774, medium italic',
+      fontClass: 'PSBasic1200-774MediumItalic',
+    },
+    {
+      id: 45,
+      fontTitle: '780, semi-bold',
+      fontClass: 'PSBasic1200-780Semi-Bold',
+    },
+    {
+      id: 46,
+      fontTitle: '784, semi-bold italic',
+      fontClass: 'PSBasic1200-784Semi-BoldItalic',
+    },
+    {
+      id: 47,
+      fontTitle: '790, bold',
+      fontClass: 'PSBasic1200-790Bold',
+    },
+    {
+      id: 48,
+      fontTitle: '794, bold italic',
+      fontClass: 'PSBasic1200-794BoldItalic',
+    },
+  ]);
+  const [wideFonts] = useState([
+    {
+      id: 49,
+      fontTitle: '840, light',
+      fontClass: '',
+    },
+    {
+      id: 50,
+      fontTitle: '844, light italic',
+      fontClass: '',
+    },
+    {
+      id: 51,
+      fontTitle: '850, semi-light',
+      fontClass: '',
+    },
+    {
+      id: 52,
+      fontTitle: '854, semi-light italic',
+      fontClass: '',
+    },
+    {
+      id: 53,
+      fontTitle: '860, regular',
+      fontClass: '',
+    },
+    {
+      id: 54,
+      fontTitle: '864, regular italic',
+      fontClass: '',
+    },
+    {
+      id: 55,
+      fontTitle: '870, medium',
+      fontClass: '',
+    },
+    {
+      id: 56,
+      fontTitle: '874, medium italic',
+      fontClass: '',
+    },
+    {
+      id: 57,
+      fontTitle: '880, semi-bold',
+      fontClass: '',
+    },
+    {
+      id: 58,
+      fontTitle: '884, semi-bold italic',
+      fontClass: '',
+    },
+    {
+      id: 59,
+      fontTitle: '890, bold',
+      fontClass: '',
+    },
+    {
+      id: 60,
+      fontTitle: '894, bold italic',
+      fontClass: '',
+    },
+  ]);
+  const [monoFonts] = useState([
+    {
+      id: 61,
+      fontTitle: '940, light',
+      fontClass: '',
+    },
+    {
+      id: 62,
+      fontTitle: '944, light italic',
+      fontClass: '',
+    },
+    {
+      id: 63,
+      fontTitle: '960, regular',
+      fontClass: '',
+    },
+    {
+      id: 64,
+      fontTitle: '964, regular italic',
+      fontClass: '',
+    },
+    {
+      id: 65,
+      fontTitle: '990, bold',
+      fontClass: '',
+    },
+    {
+      id: 66,
+      fontTitle: '994, bold italic',
+      fontClass: '',
     },
   ]);
 
@@ -170,13 +378,13 @@ const DropdownMenu = ({ className = '', toggleWeight }) => {
         {!text ? '670 - medium' : text}
       </summary>
       <div className="font-menu">
-        <div className="col">
-          <div className="grid grid-cols-3 gap-20">
-            <div>
+        <div className="col pointer-events-none text-[#6C6C6C]">
+          <div className="flex flex-row relative">
+            <div className="w-1/4 absolute top-0.5">
               <div>Narrow</div>
               <div>400</div>
             </div>
-            <div className="col-span-2">
+            <div className="w-3/4 absolute left-[90px]">
               <ul className="mt-1 [&_li]:cursor-pointer">
                 {narrowFonts.map((item) => (
                   <li key={item.id}>
@@ -191,7 +399,7 @@ const DropdownMenu = ({ className = '', toggleWeight }) => {
             </div>
           </div>
         </div>
-        <div className="col">
+        <div className="col pointer-events-none text-[#6C6C6C]">
           <div className="grid grid-cols-3 gap-20">
             <div>
               <div>Compact</div>
@@ -199,18 +407,15 @@ const DropdownMenu = ({ className = '', toggleWeight }) => {
             </div>
             <div className="col-span-2">
               <ul className="mt-1">
-                <li>540, light</li>
-                <li>544, light italic</li>
-                <li>550, semi-light</li>
-                <li>554, semi-light italic</li>
-                <li>560, regular</li>
-                <li>564, regular italic</li>
-                <li>570, medium</li>
-                <li>574, medium italic</li>
-                <li>580, semi-bold</li>
-                <li>584, semi-bold italic</li>
-                <li>590, bold</li>
-                <li>594, bold italic</li>
+                {compactFonts.map((item) => (
+                  <li key={item.id}>
+                    <FontLink
+                      {...item}
+                      isActive={active === item.id}
+                      setLinkActive={setLinkActive}
+                    />
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
@@ -244,126 +449,34 @@ const DropdownMenu = ({ className = '', toggleWeight }) => {
           </div>
         </div>
         <div className="col">
-          <div className="grid grid-cols-3 gap-20">
-            <div>
+          <div className="flex flex-row relative">
+            <div className="w-1/4 absolute top-0.5">
               <div>Extended</div>
               <div>700</div>
             </div>
-            <div className="col-span-2">
+            <div className="w-3/4 absolute left-[90px]">
               <ul className="mt-1 [&_li]:cursor-pointer">
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-740Light');
-                    changeValueAndCloseMenu({ text: '740 - light' });
-                    onToggle(e);
-                  }}
-                >
-                  740, light
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-744LightItalic');
-                    changeValueAndCloseMenu({ text: '744 - light italic' });
-                    onToggle(e);
-                  }}
-                >
-                  744, light italic
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-750Semi-Light');
-                    changeValueAndCloseMenu({ text: '750 - semi-light' });
-                    onToggle(e);
-                  }}
-                >
-                  750, semi-light
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-754Semi-LightItalic');
-                    changeValueAndCloseMenu({ text: '754 - semi-light italic' });
-                    onToggle(e);
-                  }}
-                >
-                  754, semi-light italic
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-760Regular');
-                    changeValueAndCloseMenu({ text: '760 - regular' });
-                    onToggle(e);
-                  }}
-                >
-                  760, regular
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-764RegularItalic');
-                    changeValueAndCloseMenu({ text: '764 - regular italic' });
-                    onToggle(e);
-                  }}
-                >
-                  764, regular italic
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-770Medium');
-                    changeValueAndCloseMenu({ text: '770 - medium' });
-                    onToggle(e);
-                  }}
-                >
-                  770, medium
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-774MediumItalic');
-                    changeValueAndCloseMenu({ text: '774 - medium' });
-                    onToggle(e);
-                  }}
-                >
-                  774, medium italic
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-780Semi-Bold');
-                    changeValueAndCloseMenu({ text: '780 - semi-bold' });
-                    onToggle(e);
-                  }}
-                >
-                  780, semi-bold
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-784Semi-BoldItalic');
-                    changeValueAndCloseMenu({ text: '784 - semi-bold italic' });
-                    onToggle(e);
-                  }}
-                >
-                  784, semi-bold italic
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-790Bold');
-                    changeValueAndCloseMenu({ text: '790 - bold' });
-                    onToggle(e);
-                  }}
-                >
-                  790, bold
-                </li>
-                <li
-                  onClick={(e) => {
-                    toggleWeight('PSBasic1200-794BoldItalic');
-                    changeValueAndCloseMenu({ text: '790 - bold italic' });
-                    onToggle(e);
-                  }}
-                >
-                  790, bold italic
-                </li>
+                {extendedFonts.map((item) => (
+                  <li
+                    onClick={(e) => {
+                      toggleWeight(item.fontClass);
+                      changeValueAndCloseMenu({ text: item.fontTitle });
+                      onToggle(e);
+                    }}
+                    key={item.id}
+                  >
+                    <FontLink
+                      {...item}
+                      isActive={active === item.id}
+                      setLinkActive={setLinkActive}
+                    />
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
-        <div className="col">
+        <div className="col pointer-events-none text-[#6C6C6C]">
           <div className="grid grid-cols-3 gap-20">
             <div>
               <div>Wide</div>
@@ -371,36 +484,50 @@ const DropdownMenu = ({ className = '', toggleWeight }) => {
             </div>
             <div className="col-span-2">
               <ul className="mt-1">
-                <li>840, light</li>
-                <li>844, light italic</li>
-                <li>850, semi-light</li>
-                <li>854, semi-light italic</li>
-                <li>860, regular</li>
-                <li>864, regular italic</li>
-                <li>870, medium</li>
-                <li>874, medium italic</li>
-                <li>880, semi-bold</li>
-                <li>884, semi-bold italic</li>
-                <li>890, bold</li>
-                <li>894, bold italic</li>
+                {wideFonts.map((item) => (
+                  <li
+                    onClick={(e) => {
+                      toggleWeight(item.fontClass);
+                      changeValueAndCloseMenu({ text: item.fontTitle });
+                      onToggle(e);
+                    }}
+                    key={item.id}
+                  >
+                    <FontLink
+                      {...item}
+                      isActive={active === item.id}
+                      setLinkActive={setLinkActive}
+                    />
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
         <div className="col">
-          <div className="grid grid-cols-3 gap-20">
+          <div className="grid grid-cols-3 gap-20 pointer-events-none text-[#6C6C6C]">
             <div>
               <div>Mono</div>
               <div>900</div>
             </div>
             <div className="col-span-2">
               <ul className="mt-1">
-                <li>940, light</li>
-                <li>944, light italic</li>
-                <li>960, regular</li>
-                <li>964, regular italic</li>
-                <li>990, bold</li>
-                <li>994, bold italic</li>
+                {monoFonts.map((item) => (
+                  <li
+                    onClick={(e) => {
+                      toggleWeight(item.fontClass);
+                      changeValueAndCloseMenu({ text: item.fontTitle });
+                      onToggle(e);
+                    }}
+                    key={item.id}
+                  >
+                    <FontLink
+                      {...item}
+                      isActive={active === item.id}
+                      setLinkActive={setLinkActive}
+                    />
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
