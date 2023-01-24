912222b9517f26276b43b6bc1dd2aac8097b796a
yano1978 <yano1978@gmail.com>
2023-01-24 10:18:38 +0000

    Build configuration URL changed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: 'https://ernestoianuario.com/ps-basic/',
+  // base: 'http://webdevcorso.altervista.org/ps-basic/',
   plugins: [react()],
   resolve: {
     alias: {
