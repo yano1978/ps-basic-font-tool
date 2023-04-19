d3f5076ce695f19c0c059289733aed9f59cb7dc7
yano1978 <yano1978@gmail.com>
2023-04-19 23:25:54 +0100

    Text selection added

--- src/App.jsx
+++ src/App.jsx
@@ -306,7 +306,7 @@ The design of this font is aimed at combining elements of both fonts, systematic
       >
         <textarea
           spellCheck="false"
-          className={`textarea w-full max-w-[320px] min-h-full xs:max-w-full px-4 ${weight} ${
+          className={`w-full max-w-[320px] min-h-full xs:max-w-full px-4 ${changeDeepColor} ${weight} ${
             enabled ? 'ligatures' : ''
           }`}
           style={{
